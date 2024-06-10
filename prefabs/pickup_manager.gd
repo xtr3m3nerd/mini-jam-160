746132488fbaae04eeee676752fde02b7eb74934
Xavier Vargas <xtr3m3nerd@gmail.com>
2024-06-09 22:03:38 -0500

    pickup/dropoff

--- prefabs/pickup_manager.gd
+++ prefabs/pickup_manager.gd
@@ -2,8 +2,12 @@ extends Node3D
 class_name PickupManager
 
 @onready var ray_cast_3d = $RayCast3D as RayCast3D
+@onready var color_rect = $CanvasLayer/ColorRect
 
 var pickup_in_hand = null
+
+func _ready():
+	color_rect.hide()
 
 func pickup(input_just_pressed: bool):
 	if input_just_pressed:
@@ -13,9 +17,11 @@ func pickup(input_just_pressed: bool):
 			if ray_cast_3d.get_collider().has_method("pickup") and pickup_in_hand == null:
 				pickup_in_hand = ray_cast_3d.get_collider().pickup()
 				print(pickup_in_hand)
+				color_rect.show()
 				
 			if pickup_in_hand != null and ray_cast_3d.get_collider().has_method("dropoff"):
 				ray_cast_3d.get_collider().dropoff(pickup_in_hand)
 				pickup_in_hand = null
+				color_rect.hide()
 		
 		ray_cast_3d.enabled = false
